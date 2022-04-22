e965b4ebbddf37860b2d87d77574b3980cf52b00
juniorleaoo <juniorleaoo@hotmail.com>
2022-04-21 21:48:50 -0300

    submit 'rna transcription'

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const DNA = {
+  'G': 'C',
+  'C': 'G',
+  'T': 'A',
+  'A': 'U'
+}
+
+export const toRna = (dna) => {
+  return [...dna].map(nucleotide => DNA[nucleotide]).join('')
+};
