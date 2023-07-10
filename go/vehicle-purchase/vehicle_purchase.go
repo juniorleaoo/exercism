7a4d29eff2dad658650331582f6d63e30776fb16
juniorleaoo <juniorleaoo@hotmail.com>
2023-07-09 21:46:14 -0300

    exercism(go): vehicle_purchase

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,28 @@
+package purchase
+
+import "fmt"
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	vehicle := option1
+	if option1 > option2 {
+		vehicle = option2
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", vehicle)
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return originalPrice * 0.8
+	} else if age >= 3 && age < 10 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.5
+	}
+}
